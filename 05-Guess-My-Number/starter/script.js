399d8cfeeffb2cf32fccd58eeed9185c4135dfe6
Tobias Konerding <tobias.konerding@gmail.com>
2024-06-10 21:26:23 +0200

    Finish section 7 exercise 5

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,97 @@
 'use strict';
+
+/* console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+ */
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.btn.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //   console.log(guess, typeof guess);
+
+  // Versuch ungleich
+  if (!guess) {
+    // document.querySelector('.message').textContent = '❌ No number ...';
+    displayMessage('❌ No number ...');
+
+    //Versuch gleich
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🎉 Correct Number';
+    displayMessage('🎉 Correct Number');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈 Too high' : '📉 Too low';
+      displayMessage(guess > secretNumber ? '📈 Too high' : '📉 Too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent =
+      //   '❌ You lost the game...';
+      displayMessage('❌ You lost the game...');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  /*   //Versuch zu hoch
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '❌ You lost the game...';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //Versuch zu klein
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '❌ You lost the game...';
+      document.querySelector('.score').textContent = 0;
+    }
+  } */
+});
+
+/* Restart-again Function
+1. Auswahl Again-Button + Click-Event
+2. Neue Secret-Number + "?" + Breite wieder 15rem
+3. Counter 20
+4. Background - grau
+5. Eingabe leeren + Text auf "Start Guessing"
+ */
+
+document.querySelector('.again').addEventListener('click', () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '';
+
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '';
+  document.querySelector('.guess').value = '';
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+});
